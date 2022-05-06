a3e3c68b0ca15a31df876646ad82da808dc82933
GabrielFRails <gabriel.estudy.reis@gmail.com>
2022-05-06 18:59:47 -0300

    Remove unnecessary scripts

--- scripts/install_customs.sh
+++
@@ -1,8 +0,0 @@
-printf 'Running apt-get update, wait a moment \n'
-sudo apt-get update
-cd $HOME
-printf 'back to HOME, checkin...  '
-pwd
-git clone https://github.com/vinceliuice/vimix-icon-theme.git
-cd vimix-icon-theme/
-./install.sh -a
