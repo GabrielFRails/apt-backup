f54d834cac0dcccea1fdc922545379276e9f7b75
GabrielFRails <gabriel.estudy.reis@gmail.com>
2022-07-01 09:47:29 -0300

    Update ssh script to draw attention to the bash window instead of open a new terminal

--- ssh_config.sh
+++ ssh_config.sh
@@ -10,7 +10,7 @@ cd $HOME
 printf "Host ${external_ip}\n\tHost ${external_ip}\n\tUser ${user}\n" > .ssh/config
 
 code /home/gabriel_freitas/projetos/script_bash/
-sleep 2
-gnome-terminal -- ssh ${user}@${external_ip}
-
-exit
+sleep 3
+clear
+wmctrl -a gabriel_freitas@freitas-nitro5: ~
+ssh ${user}@${external_ip}
