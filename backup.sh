b18feb6ec034de0b83f88e00a1d7e0fc598949e5
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-02-21 09:36:08 -0300

    Update google chrome install way

--- backup.sh
+++ backup.sh
@@ -3,14 +3,20 @@
 set_basic_environment()
 {
    sudo apt-get update
-   sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-   wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-   sudo apt-get update
-   sudo apt-get install google-chrome-stable -y
+   sudo apt-get upgrade
+   #sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+   #wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+   #sudo apt-get update
+   #sudo apt-get install google-chrome-stable -y
 
    sudo apt install wget -y
    sudo apt install vim -y
    sudo apt install tig -y
+   sudo apt install curl -y
+
+   curl -o  chrome_package.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+   sudo dpkg -i chrome_package.deb
+   sudo rm -r chrome_package.deb
 }
 
 customizations()
