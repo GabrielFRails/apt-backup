2016b2c1bb86781c429031df0404b4e2783edd3e
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-06-08 12:39:34 -0300

    Update docker installation

--- backup.sh
+++ backup.sh
@@ -57,12 +57,22 @@ set_dev_environment()
    sudo npm install -g @vue/cli
    npm install @nuxtjs/vuetify -D
 
+   # Add Docker's official GPG key:
    sudo apt-get update
-   sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(. /etc/os-release; echo "$UBUNTU_CODENAME") stable"
+   sudo apt-get install ca-certificates curl
+   sudo install -m 0755 -d /etc/apt/keyrings
+   sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+   sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+   # Add the repository to Apt sources:
+   echo \
+   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
    sudo apt-get update
-   sudo apt-get -y install docker-ce docker-compose
+
+   sudo apt-get update
+   sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
    sudo usermod -aG docker $USER
 }
 
