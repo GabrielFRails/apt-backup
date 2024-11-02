213a662fba8edc5e0d0f5e9124fe7787980cc04e
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-11-02 10:07:02 -0300

    Add script to install docker

--- backup.sh
+++ backup.sh
@@ -42,10 +42,43 @@ set_dev_environment()
    sudo snap install gh
 }
 
+install_docker()
+{
+   sudo apt update || { echo "Erro na atualização do apt"; exit 1; }
+
+   sudo apt install -y apt-transport-https ca-certificates curl software-properties-common || { echo "Falha na instalação de dependências"; exit 1;}
+
+   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg || { echo "Falha ao adicionar a chave GPG"; exit 1; }
+
+   echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null || { echo "Falha ao adicionar repositório do Docker"; exit 1; }
+
+   sudo apt update || { echo "Erro na atualização após adicionar o repositório do Docker"; exit 1; }
+
+   sudo apt install -y docker-ce docker-ce-cli containerd.io || { echo "Falha na instalação do Docker CE"; exit 1; }
+
+   while true; do
+      if sudo systemctl is-active --quiet docker; then
+          echo "Docker está ativo!"
+          break
+      else
+          echo "Aguardando o Docker iniciar..."
+          sleep 2
+      fi
+   done
+
+   sudo usermod -aG docker ${USER} || { echo "Falha ao adicionar usuário ao grupo docker"; exit 1; }
+
+   sudo curl -L -o /usr/local/bin/docker-compose "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" || { echo "Falha ao baixar o Docker Compose"; exit 1; }
+   sudo chmod +x /usr/local/bin/docker-compose || { echo "Falha ao configurar permissões do Docker Compose"; exit 1; }
+
+   docker-compose --version || { echo "Falha na verificação da versão do Docker Compose"; exit 1; }
+}
+
 runall()
 {
    set_basic_environment
    set_dev_environment
+   install_docker
 }
 
 runall
